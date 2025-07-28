307299300b238c010bbd4ed7f83163ff2e3b3aba
Leandro LeHe <141201325+Leandro-LeHe@users.noreply.github.com>
2025-07-28 14:43:48 -0400

    Add files via upload

---
+++ Demo2-API/src/main/java/Demo_API/Demo_API/DemoApiApplication.java
@@ -0,0 +1,13 @@
+package Demo_API.Demo_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApiApplication.class, args);
+	}
+
+}
---
+++ Demo2-API/src/test/java/Demo_API/Demo_API/DemoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package Demo_API.Demo_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
