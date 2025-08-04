02e71e4d019ccde0bf324c568a81dfcd96e540a4
Leandro Henrique Gonçalves da Silva <leandroh@defensoria.ms.def.br>
2025-08-04 14:19:32 -0400

    Repositories, Interfaces, Mappers
    dev

--- Demo2-API/src/main/java/Demo_API/Demo_API/Repository/AssistidosRepository.java
+++ Demo2-API/src/main/java/Demo_API/Demo_API/Repository/AssistidosRepository.java
@@ -4,13 +4,15 @@ import Demo_API.Demo_API.Model.AssistidosEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+
 
 @Repository
-//indica que vamos trabalhar com a classe AssistidosEntity e a chave primaria é um long
+
 // JpaRepository tem todos on metodos para trabalhar com banco de dados
 public interface AssistidosRepository extends JpaRepository<AssistidosEntity,Long> {
-   // List<AssistidosEntity> BuscarId(Long id);
+
+   // (pode criar métodos personalizados)
+   //List<AssistidosEntity> findByCidadeAndIdade(String cidade, int idade);
 
     Long id(Long id);
 }
